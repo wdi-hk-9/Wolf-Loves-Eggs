29ac23d97dc0728345362eb94cfd69267a374c53
AA <andrewhkg@hotmail.com>
2015-10-12 17:29:24 +0800

    created logic in application.js for keyup X,S,A,Z

--- JS/application.js
+++ JS/application.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 
   var score = 0;
+  // 0 == top left
+  // 1 == top right
+  // 2 == bottom left
+  // 3 == bottom right
+  var wolfPosition = 0;
 
-  // Generate random 4 numbers in an interval for falling eggs
-  var eggRand = function(){
-    return Math.floor(Math.random() * 4);
-    console.log(eggRand);
-  };
-
-  // activate Button click
   $("#start").on('click', function(){
     console.log("clecked btn");
+  });
+
+  // listen to keyboard and:
+  // - if 'w' is pressed
+  //   --> console.log 'w was pressed'
+  //   --> change state of wolfPosition variable, console.log wolfPosition to check
+  //   --> update #wolf with corresponding picture
+  // - if 'e' is pressed --> console.log 'e was pressed'
+  // - if 's' is pressed --> console.log 's was pressed'
+  // - if 'd' is pressed --> console.log 'd was pressed'
+  //
+  $(document).on("keyup", function(){
+    console.log()
   });
 
 });
