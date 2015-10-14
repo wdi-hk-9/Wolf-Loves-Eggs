4dd1b3f63027ef529fb6daa68cb39f5abd1ed75a
AA <andrewhkg@hotmail.com>
2015-10-14 17:55:48 +0800

    set all logic in js file as counting catch eggs, dropped eggs, set max drop eggs = 4 and game is over

--- JS/application.js
+++ JS/application.js
@@ -1,115 +1,146 @@
 $(document).ready(function(){
 
   var score = 0;
-  // 0 == top left
-  // 1 == top right
-  // 2 == bottom left
-  // 3 == bottom right
+  var lostEggs = 0;
   var wolfPosition = 0;
+  var gameTimer;
 
-  $("#start").on('click', function(whereEggGo){
+  var Egg = function() {
+    this.CSS = [
+      'egg-left  egg-topL',    // 0 = Left-Up
+      'egg-left  egg-bottom',  // 1 = Left-Bottom
+      'egg-right egg-topR',    // 2 = Right-Up
+      'egg-right egg-bottom',  // 3 = Right-Bottom
+    ];
 
-    console.log("clicked btn");
+    this.position = Math.floor(Math.random() *4);
+    //this.position = 0;
+    // Step of the animation
+    // 0 = initial egg
+    // 1 = go horizontal in branch
+    // 2 = fall down to where it can be catched
+    // 3 = fall down to ground
+    // 4 = change image to broken egg
+    this.step = 0;
+    this.timer = 0;
+    this.element;
+  };
 
+  Egg.prototype.start = function() {
+    // Add egg to the screeen
+    $('#game').append('<img class="egg" src="img/egg-first.png">');
+    // Save the jQuery element in this.$element to use later
+    this.element = $('#game :last-child');
+    // Position element in screen
+    this.element.addClass(this.CSS[this.position]);
+    // Start animation timer
+    if (this.position < 2) {
+      this.timer = setTimeout(this.animateL(this), 1000);
+    } else {
+      this.timer = setTimeout(this.animateR(this), 1000);
+    }
+  };
+
+  Egg.prototype.animateL = function(egg) {
+    // Horizontal animation
+    $(egg.element).animate({left: "+=60"}, 1000, function() {
+      // Diagonal animation
+      $(this).animate({left: "+=85", top: "+=60"}, 1000, function(){
+        // Vertical animation
+        $(this).animate({top: "+=30"}, 500, function(){
+          // if wolf: stop and delete egg, increase counter
+          if (wolfPosition == egg.position) {
+            $(this).remove();
+            score = score +1;
+            console.log('you got the egg');
+          } else {
+            // if not wolf: animate down
+            $(this).animate({top: "+=150", width: "30px"}, 1000, function(){
+              $(this).attr('src', 'img/egg-broken-30percent.png');
+              // change to broken
+              lostEggs = lostEggs + 1;
+              if (lostEggs > 4) {
+                $('.egg').remove();
+                console.log('game over');
+                clearInterval(gameTimer);
+              } else {
+                console.log('you lost the egg');
+                $('#LostEgg').html(lostEggs);
+              }
+            });
+          }
+          $('#scoreEgg').html(score);
+
+        });
+      });
+    });
+  };
+
+  //animation of right side
+  Egg.prototype.animateR = function(egg) {
+    // Horizontal animation
+    $(egg.element).animate({left: "-=60"}, 1000, function() {
+      // Diagonal animation
+      $(this).animate({left: "-=85", top: "+=60"}, 1000, function(){
+        // Vertical animation
+        $(this).animate({top: "+=30"}, 500, function(){
+          // if wolf: stop and delete egg, increase counter
+          if (wolfPosition == egg.position) {
+            $(this).remove();
+            score = score +1;
+            console.log('you got the egg');
+          } else {
+            // if not wolf: animate down
+            $(this).animate({top: "+=145", width: "30px"}, 1000, function(){
+              $(this).attr('src', 'img/egg-broken-30percent.png');
+              // change to broken
+              lostEggs = lostEggs + 1;
+              if (lostEggs > 4) {
+                console.log('game over');
+                $('.egg').remove();
+                clearInterval(gameTimer);
+              } else {
+                console.log('you lost the egg');
+                $('#LostEgg').html(lostEggs);
+              }
+            });
+          }
+          $('#scoreEgg').html(score);
+
+        });
+      });
+    });
+  };
+
+  var generateEggs = function() {
+    var newEgg = new Egg();
+    newEgg.start();
+  };
+
+  $("#start").on('click', function(){
+    gameTimer = setInterval(generateEggs, 1000);
   });
 
   // listen to keyboard and:
-  // - if 'w' is pressed
-  //   --> console.log 'w was pressed'
-  //   --> change state of wolfPosition variable, console.log wolfPosition to check
   //   --> update #wolf with corresponding picture
-  // - if 'e' is pressed --> console.log 'e was pressed'
-  // - if 's' is pressed --> console.log 's was pressed'
-  // - if 'd' is pressed --> console.log 'd was pressed'
-  //
   $(document).on("keyup", function(){
-  var keycode = (event.keyCode ? event.keyCode : event.which);
+    var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '90'){
       $('#wolf').attr('src','img/lb.png');
-        console.log('You pressed a "Z" key in keaboard');
+      wolfPosition = 1;
     }
     else if (keycode == '65'){
-
       $('#wolf').attr('src','img/lu.png');
-        console.log('You pressed a "A" key in keaboard');
+      wolfPosition = 0;
     }
     else if (keycode == '88'){
-
-      $('#wolf').attr('src','img/rb.png');
-        console.log('You pressed a "X" key in keaboard');
+      $('#wolf').attr('src','img/rbcopy.png');
+      wolfPosition = 3;
     }
     else if (keycode == '83'){
-
-      $('#wolf').attr('src','img/ru.png');
-        console.log('You pressed a "S" key in keaboard');
+      $('#wolf').attr('src','img/rucopy.png');
+      wolfPosition = 2;
     }
   });
-//----------------------------------------------------------
-// set random number for falling eggs for 4 sides brunches
-// --> if random number = 0 ---> assign to left upper brunch
-// --> if random number = 1 ---> assign to left low brunch
-// --> if random number = 2 ---> assign to right upper brunch
-// --> if random number = 3 ---> assign to right low brunch
-
-
-//--------------------------------------------------------
-//set function timeout 5 sec for falling new eggs to random branches
-// var timeoutID = setTimeout (whereEggGo, 5000);
-var timeoutID = setTimeout (whereEggGo, 1000);
-
-  var randEgg = Math.floor(Math.random() *4);
-
-  function whereEggGo(){
-    var newRandEgg = randEgg;
-    if (newRandEgg == 0) {
-      $('#branch-LU').attr('src', 'img/tree-L1.png');
-
-      console.log("adding time out");
-      setTimeout(animeEgg, 500);
-
-      function animeEgg(){
-
-        // set the first image
-        $('#branch-LU').attr('src', 'img/tree-L1.png');
-
-        // setTimeout(function(){$('#branch-LU').attr('src', 'img/tree-L1.png');}, 500);
-
-        setTimeout(function(){$('#branch-LU').attr('src', 'img/tree-L2.png');}, 1000);
-        setTimeout(function(){$('#branch-LU').attr('src', 'img/tree-L3.png');}, 1500);
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L4.png');}, 2000);
-        setTimeout(function(){$('#branch-L-Broken').attr('src', 'img/egg-broken-30percent.png');}, 2500);
-        setTimeout(function(){$('#branch-LU').attr('src', 'img/tree-L.png');}, 2200);
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L.png');}, 2500);
-      }
-
-
-    } else if (newRandEgg == 1){
-      $('#branch-LB').attr('src', 'img/tree-L1.png');
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L2.png');}, 1000);
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L3.png');}, 1500);
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L4.png');}, 2000);
-        setTimeout(function(){$('#branch-L-Broken').attr('src', 'img/egg-broken-30percent.png');}, 2500);
-        setTimeout(function(){$('#branch-LU').attr('src', 'img/tree-L.png');}, 2200);
-        setTimeout(function(){$('#branch-LB').attr('src', 'img/tree-L.png');}, 2500);
-    } else if (newRandEgg == 2){
-      $('#branch-RU').attr('src', 'img/tree-R1.png');
-        setTimeout(function(){$('#branch-RU').attr('src', 'img/tree-R2.png');}, 1000);
-        setTimeout(function(){$('#branch-RU').attr('src', 'img/tree-R3.png');}, 1500);
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R4.png');}, 2000);
-        setTimeout(function(){$('#branch-R-Broken').attr('src', 'img/egg-broken-30percent.png');}, 2500);
-        setTimeout(function(){$('#branch-RU').attr('src', 'img/tree-R.png');}, 2200);
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R.png');}, 2500);
-    } else if (newRandEgg == 3){
-      $('#branch-RB').attr('src', 'img/tree-R1.png');
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R2.png');}, 1000);
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R3.png');}, 1500);
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R4.png');}, 2000);
-        setTimeout(function(){$('#branch-R-Broken').attr('src', 'img/egg-broken-30percent.png');}, 2500);
-        setTimeout(function(){$('#branch-RU').attr('src', 'img/tree-R.png');}, 2200);
-        setTimeout(function(){$('#branch-RB').attr('src', 'img/tree-R.png');}, 2500);
-    }
-}
-
-
 
 });
